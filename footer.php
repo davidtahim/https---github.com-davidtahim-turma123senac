44d6e2895a3c5664cee5732c5c83f74ea885b670
David Tahim <davidtahim@gmail.com>
2023-05-11 10:53:05 -0300

    first commit

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer>
+<p>Atendimento: 
+<a href="mailto:atendimento@meuamigocaopetshop.com">
+atendimento@meuamigocaopetshop.com</a>  
+ - (0XX) 99999-9999 / (0XX) 88888-8888</p>
+</footer>
+
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,47 @@
+<?php get_header(); ?>
+<aside>
+<p><img src="multimidia/brincando.jpg" alt="anúncio"></p>
+<audio controls>
+ <source src="multimidia/brincar_com_cuidado.ogg" type='audio/ogg; codecs="vorbis"'>
+ <source src="multimidia/brincar_com_cuidado.mp3" type='audio/mpeg; codecs="mp3"'>
+</audio>
+<p>Ouça a dica do veterinário!</p>
+<p><img src="multimidia/anuncio_pet1.jpg" alt="anúncio"></p>
+</aside>
+
+
+
+<main>
+
+<figure class="anuncioAnime">
+<div class="boxAnuncio">
+<img src="multimidia/anuncio1.jpg" alt="">  
+<p>BRINCADEIRAS<br>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+</div>
+<div class="boxAnuncio">
+<img src="multimidia/anuncio2.jpg" alt="">  
+<p>CUIDADOS<br>Facilis, suscipit, expedita, sed, architecto ex illo excepturi blanditiis.</p>
+</div>
+<div class="boxAnuncio">
+<img src="multimidia/anuncio3.jpg" alt="">  
+<p>SAÚDE<br>Aliquam qui at fugit magnam possimus praesentium repudiandae sint.</p>
+</div>
+</figure>
+<h1>O melhor para o nosso amigo cão</h1>
+
+<p>Quisque venenatis urna ac nulla. Maecenas quis turpis nec nulla auctor mattis. Curabitur mi libero, scelerisque eu, cursus semper, tempor quis, ante. Etiam iaculis, nisl eu imperdiet tincidunt, neque lacus ornare leo, non ultrices arcu justo ut enim. Morbi pulvinar sagittis sapien. Sed adipiscing purus nec tortor. Cras nisi sapien, ultricies nec, dapibus ut, vehicula ac, pede. Donec venenatis lorem sit amet erat vehicula hendrerit. In convallis odio id augue. Sed nunc. Donec id tellus nec urna placerat consectetuer. Maecenas eget metus. Etiam et risus. Cras nunc mi, aliquam semper, suscipit a, dictum elementum, velit. Curabitur</p>
+
+</main>
+
+
+
+<aside>
+<video controls poster="multimidia/brincando2.jpg">
+<source src="multimidia/cao_brincando.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+<source src="multimidia/cao_brincando.ogv" type='video/ogg; codecs="theora, vorbis"'>
+</video>
+<p>Brinque sempre com seu cãozinho!</p>
+<img src="multimidia/anuncio_pet2.jpg" alt="anúncio">
+</aside>
+
+<?php get_footer(); ?>
